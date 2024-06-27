a3379785c57e44d166b8c8e5e42f93c7a7071e36
ath31st <ath31st@yandex.ru>
2024-06-27 15:59:11 +0300

    init project

---
+++ src/main/java/sidim/doma/wc/WindowCalculatorApplication.java
@@ -0,0 +1,13 @@
+package sidim.doma.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowCalculatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WindowCalculatorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sidim/doma/wc/WindowCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package sidim.doma.wc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowCalculatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
