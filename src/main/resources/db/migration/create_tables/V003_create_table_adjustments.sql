4ab664899472b2368a2749db2f386d000a9b915d
ath31st <ath31st@yandex.ru>
2024-07-02 08:47:35 +0300

    add a script for creation adjustments table

---
+++ src/main/resources/db/migration/create_tables/V003_create_table_adjustments.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cart_items
+(
+    id         SERIAL PRIMARY KEY,
+    order_id   INTEGER REFERENCES orders (id) ON DELETE CASCADE,
+    summary    TEXT,
+    price      NUMERIC(10, 2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
