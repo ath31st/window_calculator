ed96a79bea125b405ca356efde8ddfc8b0a4902f
ath31st <ath31st@yandex.ru>
2024-11-10 21:42:54 +0300

    update SQL scripts with not null restricting

--- src/main/resources/db/migration/create_tables/V003__create_table_block_tables.sql
+++ src/main/resources/db/migration/create_tables/V003__create_table_block_tables.sql
@@ -2,8 +2,8 @@ CREATE TABLE block_tables
 (
     id             SERIAL PRIMARY KEY,
     frame_block_id INTEGER REFERENCES frame_blocks (id) ON DELETE CASCADE,
-    name           VARCHAR(255),
-    button_type    INTEGER,
+    name           VARCHAR(255) NOT NULL,
+    button_type    INTEGER      NOT NULL,
     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- src/main/resources/db/migration/create_tables/V004__create_table_buttons.sql
+++ src/main/resources/db/migration/create_tables/V004__create_table_buttons.sql
@@ -2,8 +2,8 @@ CREATE TABLE table_buttons
 (
     id             SERIAL PRIMARY KEY,
     block_table_id INTEGER REFERENCES block_tables (id) ON DELETE CASCADE,
-    name           VARCHAR(255),
-    value          DECIMAL(10, 2),
+    name           VARCHAR(255)   NOT NULL,
+    value          DECIMAL(10, 2) NOT NULL,
     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
