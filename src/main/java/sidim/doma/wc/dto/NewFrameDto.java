b91a9b41d5274f1e527262cb6b7d11662a7dbb7b
ath31st <ath31st@yandex.ru>
2024-11-13 22:39:51 +0300

    add constraints for NewFrameDto

--- src/main/java/sidim/doma/wc/dto/NewFrameDto.java
+++ src/main/java/sidim/doma/wc/dto/NewFrameDto.java
@@ -1,10 +1,14 @@
 package sidim.doma.wc.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record NewFrameDto(
+    @NotEmpty(message = "Name is required and cannot be empty")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
     String name
 ) implements Serializable {
 }
