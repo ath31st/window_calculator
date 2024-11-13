ff2cf844df8ac8ab5bf8f110232f4f9899d76548
ath31st <ath31st@yandex.ru>
2024-11-13 21:45:07 +0300

    add methods and tests for creation and rename frames

--- src/main/java/sidim/doma/wc/service/FrameService.java
+++ src/main/java/sidim/doma/wc/service/FrameService.java
@@ -1,7 +1,13 @@
 package sidim.doma.wc.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import sidim.doma.wc.dto.FrameDto;
+import sidim.doma.wc.dto.NewFrameDto;
+import sidim.doma.wc.entity.Frame;
+import sidim.doma.wc.exception.FrameServiceException;
 import sidim.doma.wc.repository.FrameRepository;
 
 @Service
@@ -10,4 +16,35 @@ public class FrameService {
   private final FrameRepository frameRepository;
 
 
+  public FrameDto create(NewFrameDto frameDto) {
+    val frame = new Frame();
+    frame.setName(frameDto.name());
+
+    val savedFrame = frameRepository.save(frame);
+    return new FrameDto(savedFrame.getId(), savedFrame.getName());
+  }
+
+  public FrameDto renameFrame(String newName, Integer id) {
+    checkExistsFrame(id);
+
+    val frame = getFrame(id);
+    frame.setName(newName);
+    frameRepository.save(frame);
+
+    return new FrameDto(frame.getId(), frame.getName());
+  }
+
+  private Frame getFrame(Integer id) {
+    return frameRepository.findById(id).orElseThrow(
+        () -> new FrameServiceException(String.format("Frame with id: %d not found", id),
+            HttpStatus.NOT_FOUND)
+    );
+  }
+
+  private void checkExistsFrame(Integer frameId) {
+    if (!frameRepository.existsById(frameId)) {
+      throw new FrameServiceException(String.format("Frame with id: %d not found", frameId),
+          HttpStatus.NOT_FOUND);
+    }
+  }
 }
--- src/test/java/sidim/doma/wc/service/FrameServiceTest.java
+++ src/test/java/sidim/doma/wc/service/FrameServiceTest.java
@@ -1,10 +1,24 @@
 package sidim.doma.wc.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import sidim.doma.wc.dto.FrameDto;
+import sidim.doma.wc.dto.NewFrameDto;
+import sidim.doma.wc.entity.Frame;
+import sidim.doma.wc.exception.FrameServiceException;
 import sidim.doma.wc.repository.FrameRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,7 +30,52 @@ class FrameServiceTest {
   @InjectMocks
   private FrameService frameService;
 
+  private NewFrameDto frameDto;
+  private FrameDto expectedFrameDto;
+  private Frame frame;
+
   @BeforeEach
   void setUp() {
+    frameDto = new NewFrameDto("test");
+    frame = Frame.builder()
+        .id(1)
+        .name("test")
+        .build();
+    expectedFrameDto = new FrameDto(1, "test");
+  }
+
+  @Test
+  void createNewFrame_whenValidDataProvided() {
+    when(frameRepository.save(any(Frame.class))).thenReturn(frame);
+
+    val savedFrame = frameService.create(frameDto);
+
+    assertEquals(expectedFrameDto, savedFrame);
+    verify(frameRepository).save(argThat(f -> f.getName().equals("test")));
+  }
+
+  @Test
+  void renameFrame_whenValidDataProvided() {
+    val newName = "another_test";
+    val id = 1;
+
+    when(frameRepository.existsById(id)).thenReturn(true);
+    when(frameRepository.findById(id)).thenReturn(Optional.ofNullable(frame));
+    when(frameRepository.save(any(Frame.class))).thenReturn(frame);
+
+    val savedFrame = frameService.renameFrame(newName, id);
+
+    assertEquals(newName, savedFrame.name());
+  }
+
+  @Test
+  void renameFrame_whenFrameNotFound_thenThrow() {
+    val newName = "another_test";
+    val id = 1;
+
+    when(frameRepository.existsById(id)).thenReturn(false);
+
+    assertThrows(FrameServiceException.class, () -> frameService.renameFrame(newName, id));
+    verify(frameRepository).existsById(id);
   }
 }
