908756953fea3c9d4878c8e691c35d1a5c4d6120
ath31st <ath31st@yandex.ru>
2024-11-15 21:13:23 +0300

    update creation frame tests

--- src/test/java/sidim/doma/wc/controller/FrameControllerTest.java
+++ src/test/java/sidim/doma/wc/controller/FrameControllerTest.java
@@ -5,11 +5,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import sidim.doma.wc.dto.FrameDto;
 import sidim.doma.wc.dto.NewFrameDto;
@@ -24,25 +28,33 @@ class FrameControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  private ObjectMapper objectMapper;
+
   @Test
   void createNewFrame_whenValidDataProvided() throws Exception {
-    val newFrameDto = new NewFrameDto("test");
+    val name = "test";
+    val id = 1;
+    val newFrameDto = new NewFrameDto(name);
 
-    when(frameService.createFrame(newFrameDto)).thenReturn(new FrameDto(1, "test"));
+    when(frameService.createFrame(newFrameDto)).thenReturn(new FrameDto(id, name));
 
     mockMvc.perform(post("/api/v1/frames")
-            .contentType("application/json")
-            .content("{\"name\":\"test\"}"))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(newFrameDto)))
         .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.id").value(1))
-        .andExpect(jsonPath("$.name").value("test"));
+        .andExpect(jsonPath("$.id").value(id))
+        .andExpect(jsonPath("$.name").value(name));
   }
 
-  @Test
-  void createNewFrame_whenInvalidDataProvided() throws Exception {
+  @ParameterizedTest
+  @ValueSource(strings = {"", "  ", "sh"})
+  void createNewFrame_whenInvalidDataProvided(String emptyName) throws Exception {
+    val newFrameDto = new NewFrameDto(emptyName);
+
     mockMvc.perform(post("/api/v1/frames")
-            .contentType("application/json")
-            .content("{\"name\":\"\"}"))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(newFrameDto)))
         .andExpect(status().isBadRequest());
   }
 }
