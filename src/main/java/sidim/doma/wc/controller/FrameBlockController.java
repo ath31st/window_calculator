e5d9778e7048cb203572edcbc380fbb7625100c5
ath31st <ath31st@yandex.ru>
2024-11-16 14:27:14 +0300

    update frame block controller with test

--- src/main/java/sidim/doma/wc/controller/FrameBlockController.java
+++ src/main/java/sidim/doma/wc/controller/FrameBlockController.java
@@ -1,11 +1,31 @@
 package sidim.doma.wc.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sidim.doma.wc.dto.frame_block.FrameBlockDto;
+import sidim.doma.wc.dto.frame_block.NewFrameBlockDto;
+import sidim.doma.wc.service.FrameBlockService;
+import sidim.doma.wc.service.FrameService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/frame_blocks")
+@RequestMapping("/api/v1")
 public class FrameBlockController {
+  private final FrameBlockService frameBlockService;
+  private final FrameService frameService;
+
+  @PostMapping("/frame_blocks")
+  public ResponseEntity<FrameBlockDto> createFrameBlock(@RequestBody @Valid NewFrameBlockDto dto) {
+    val frame = frameService.getFrame(dto.frameId());
+    val savedFrameBlockDto = frameBlockService.createFrameBlock(dto, frame);
+
+    return new ResponseEntity<>(savedFrameBlockDto, HttpStatus.CREATED);
+  }
 }
--- src/test/java/sidim/doma/wc/controller/FrameBlockControllerTest.java
+++ src/test/java/sidim/doma/wc/controller/FrameBlockControllerTest.java
@@ -3,25 +3,35 @@ package sidim.doma.wc.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import sidim.doma.wc.dto.frame_block.FrameBlockDto;
 import sidim.doma.wc.dto.frame_block.NewFrameBlockDto;
+import sidim.doma.wc.entity.Frame;
+import sidim.doma.wc.service.FrameBlockService;
+import sidim.doma.wc.service.FrameService;
 
 @WebMvcTest(controllers = FrameBlockController.class)
 class FrameBlockControllerTest {
 
   private static final String BASE_URL = "/api/v1/frame_blocks";
+
+  @MockBean
+  private FrameBlockService frameBlockService;
+
+  @MockBean
+  private FrameService frameService;
 
   @Autowired
   private MockMvc mockMvc;
@@ -29,21 +39,28 @@ class FrameBlockControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
+  private final Integer frameId = 1;
+  private Integer blockId = 2;
+  private final String name = "test_block";
+  private final Boolean isWindowSizeEnabled = false;
+  private final String blockInput = "test_block_input";
+  private Frame frame;
+  private NewFrameBlockDto newFrameBlockDto;
+  private FrameBlockDto frameBlockDto;
+
   @BeforeEach
   void setUp() {
-  }
+    newFrameBlockDto = new NewFrameBlockDto(frameId, name, isWindowSizeEnabled, blockInput, null);
+    frameBlockDto = new FrameBlockDto(blockId, name, isWindowSizeEnabled, blockInput, null);
 
-  @AfterEach
-  void tearDown() {
+    frame = new Frame();
+    frame.setId(frameId);
   }
 
   @Test
   void createFrameBlock_success() throws Exception {
-    val frameId = 1;
-    val name = "test_block";
-    val isWindowSizeEnabled = false;
-    val blockInput = "test_block_input";
-    val newFrameBlockDto = new NewFrameBlockDto(frameId, name, isWindowSizeEnabled, blockInput, null);
+    when(frameService.getFrame(frameId)).thenReturn(frame);
+    when(frameBlockService.createFrameBlock(newFrameBlockDto, frame)).thenReturn(frameBlockDto);
 
     val mvcResult = mockMvc.perform(post(BASE_URL)
             .contentType(MediaType.APPLICATION_JSON)
@@ -51,12 +68,12 @@ class FrameBlockControllerTest {
         .andExpect(status().isCreated()).andReturn();
 
     val content = mvcResult.getResponse().getContentAsString();
-    val frameBLockDto = objectMapper.readValue(content, FrameBlockDto.class);
+    val blockDto = objectMapper.readValue(content, FrameBlockDto.class);
 
-    assertNotNull(frameBLockDto.id());
-    assertEquals(name, frameBLockDto.name());
-    assertEquals(blockInput, frameBLockDto.inputTitle());
-    assertEquals(isWindowSizeEnabled, frameBLockDto.isWindowSizeEnabled());
-    assertNull(frameBLockDto.description());
+    assertNotNull(blockDto.id());
+    assertEquals(name, blockDto.name());
+    assertEquals(blockInput, blockDto.inputTitle());
+    assertEquals(isWindowSizeEnabled, blockDto.isWindowSizeEnabled());
+    assertNull(blockDto.description());
   }
 }
