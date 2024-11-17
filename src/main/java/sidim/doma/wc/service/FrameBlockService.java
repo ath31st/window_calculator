8f4f7f8caeea3261444dffacd28fd3132e5e17b9
ath31st <ath31st@yandex.ru>
2024-11-17 09:42:12 +0300

    add delete/update tests for FrameBlockService

--- src/main/java/sidim/doma/wc/service/FrameBlockService.java
+++ src/main/java/sidim/doma/wc/service/FrameBlockService.java
@@ -2,10 +2,14 @@ package sidim.doma.wc.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import sidim.doma.wc.dto.frame_block.FrameBlockDto;
 import sidim.doma.wc.dto.frame_block.NewFrameBlockDto;
+import sidim.doma.wc.dto.frame_block.UpdateFrameBlockDto;
 import sidim.doma.wc.entity.Frame;
+import sidim.doma.wc.entity.FrameBlock;
+import sidim.doma.wc.exception.FrameBlockServiceException;
 import sidim.doma.wc.mapper.FrameBlockMapper;
 import sidim.doma.wc.repository.FrameBlockRepository;
 
@@ -20,5 +24,38 @@ public class FrameBlockService {
 
     val savedFrameBlock = frameBlockRepository.save(frameBlock);
     return frameBlockMapper.fromEntityToDto(savedFrameBlock);
+  }
+
+  public void deleteFrameBlock(Integer id) {
+    checkExistsFrameBlock(id);
+
+    frameBlockRepository.deleteById(id);
+  }
+
+  private void checkExistsFrameBlock(Integer id) {
+    if (!frameBlockRepository.existsById(id)) {
+      throw new FrameBlockServiceException(String.format("FrameBlock with id: %d not found", id),
+          HttpStatus.NOT_FOUND);
+    }
+  }
+
+  public FrameBlockDto updateFrameBlock(UpdateFrameBlockDto dto) {
+    checkExistsFrameBlock(dto.id());
+
+    val frameBlock = getFrameBlock(dto.id());
+
+    frameBlock.setName(dto.name());
+    frameBlock.setIsWindowSizeEnabled(dto.isWindowSizeEnabled());
+    frameBlock.setInputTitle(dto.inputTitle());
+    frameBlock.setDescription(dto.description());
+
+    val updatedFrameBlock = frameBlockRepository.save(frameBlock);
+    return frameBlockMapper.fromEntityToDto(updatedFrameBlock);
+  }
+
+  public FrameBlock getFrameBlock(Integer id) {
+    checkExistsFrameBlock(id);
+
+    return frameBlockRepository.findById(id).orElseThrow();
   }
 }
--- src/test/java/sidim/doma/wc/service/FrameBlockServiceTest.java
+++ src/test/java/sidim/doma/wc/service/FrameBlockServiceTest.java
@@ -1,10 +1,14 @@
 package sidim.doma.wc.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Collections;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,8 +18,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sidim.doma.wc.dto.frame_block.FrameBlockDto;
 import sidim.doma.wc.dto.frame_block.NewFrameBlockDto;
+import sidim.doma.wc.dto.frame_block.UpdateFrameBlockDto;
 import sidim.doma.wc.entity.Frame;
 import sidim.doma.wc.entity.FrameBlock;
+import sidim.doma.wc.exception.FrameBlockServiceException;
 import sidim.doma.wc.mapper.FrameBlockMapper;
 import sidim.doma.wc.repository.FrameBlockRepository;
 
@@ -31,10 +37,10 @@ class FrameBlockServiceTest {
   @InjectMocks
   private FrameBlockService frameBlockService;
 
-  private final FrameBlockMapper frameBlockMapper = new FrameBlockMapper();
-
   private NewFrameBlockDto newFrameBlockDto;
+  private UpdateFrameBlockDto updateFrameBlockDto;
   private FrameBlockDto expectedFrameBlockDto;
+  private FrameBlockDto updatedFrameBlockDto;
   private FrameBlock frameBlock;
   private Frame frame;
 
@@ -42,18 +48,27 @@ class FrameBlockServiceTest {
   void setUp() {
     val frameId = 1;
     val name = "test_block";
+    val updateName = "another_test_block";
     val isWindowSizeEnabled = false;
     val blockInput = "test_block_input";
+    val updateBlockInput = "another_test_block_input";
 
     newFrameBlockDto = new NewFrameBlockDto(frameId, name, isWindowSizeEnabled, blockInput, null);
 
     frame = new Frame();
     frame.setId(frameId);
 
-    frameBlock = frameBlockMapper.fromNewToEntity(newFrameBlockDto, frame);
-    frameBlock.setId(1);
+    frameBlock = new FrameBlock(1, frame, name, isWindowSizeEnabled, blockInput,
+        null, LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant(),
+        null, Collections.emptySet());
 
-    expectedFrameBlockDto = frameBlockMapper.fromEntityToDto(frameBlock);
+    expectedFrameBlockDto = new FrameBlockDto(frameBlock.getId(), name,
+        isWindowSizeEnabled, blockInput, null);
+    updatedFrameBlockDto = new FrameBlockDto(frameBlock.getId(),
+        updateName, isWindowSizeEnabled, updateBlockInput, null);
+
+    updateFrameBlockDto = new UpdateFrameBlockDto(frameBlock.getId(),
+        updateName, isWindowSizeEnabled, updateBlockInput, null);
   }
 
   @Test
@@ -66,5 +81,58 @@ class FrameBlockServiceTest {
 
     assertEquals(expectedFrameBlockDto, savedFrameBlockDto);
     verify(frameBlockRepository).save(any(FrameBlock.class));
+  }
+
+  @Test
+  void deleteFrameBlock_success() {
+    when(frameBlockRepository.existsById(1)).thenReturn(true);
+
+    frameBlockService.deleteFrameBlock(1);
+
+    verify(frameBlockRepository).deleteById(1);
+  }
+
+  @Test
+  void deleteFrameBlock_whenFrameBlockNotFound() {
+    when(frameBlockRepository.existsById(1)).thenReturn(false);
+
+    assertThrows(FrameBlockServiceException.class, () -> frameBlockService.deleteFrameBlock(1));
+  }
+
+  @Test
+  void updateFrameBlock_success() {
+    when(mockedMapper.fromEntityToDto(any(FrameBlock.class))).thenReturn(updatedFrameBlockDto);
+    when(frameBlockRepository.existsById(1)).thenReturn(true);
+    when(frameBlockRepository.findById(1)).thenReturn(java.util.Optional.of(frameBlock));
+    when(frameBlockRepository.save(any(FrameBlock.class))).thenReturn(frameBlock);
+
+    val savedFrameBlockDto = frameBlockService.updateFrameBlock(updateFrameBlockDto);
+
+    assertEquals(updatedFrameBlockDto, savedFrameBlockDto);
+    verify(frameBlockRepository).save(any(FrameBlock.class));
+  }
+
+  @Test
+  void updateFrameBlock_whenFrameBlockNotFound() {
+    when(frameBlockRepository.existsById(1)).thenReturn(false);
+
+    assertThrows(FrameBlockServiceException.class, () -> frameBlockService.updateFrameBlock(updateFrameBlockDto));
+  }
+
+  @Test
+  void getFrameBlock_success() {
+    when(frameBlockRepository.existsById(1)).thenReturn(true);
+    when(frameBlockRepository.findById(1)).thenReturn(java.util.Optional.of(frameBlock));
+
+    val actualFrameBlock = frameBlockService.getFrameBlock(1);
+
+    assertEquals(actualFrameBlock, frameBlock);
+  }
+
+  @Test
+  void getFrameBlock_whenFrameBlockNotFound() {
+    when(frameBlockRepository.existsById(1)).thenReturn(false);
+
+    assertThrows(FrameBlockServiceException.class, () -> frameBlockService.getFrameBlock(1));
   }
 }
