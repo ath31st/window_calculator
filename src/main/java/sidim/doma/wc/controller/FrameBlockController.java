0f016c0b92489244f53a617f17c99e6b17f735dc
ath31st <ath31st@yandex.ru>
2024-11-17 10:11:36 +0300

    add update endpoint and tests for FrameBlockController

--- src/main/java/sidim/doma/wc/controller/FrameBlockController.java
+++ src/main/java/sidim/doma/wc/controller/FrameBlockController.java
@@ -6,11 +6,13 @@ import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sidim.doma.wc.dto.frame_block.FrameBlockDto;
 import sidim.doma.wc.dto.frame_block.NewFrameBlockDto;
+import sidim.doma.wc.dto.frame_block.UpdateFrameBlockDto;
 import sidim.doma.wc.service.FrameBlockService;
 import sidim.doma.wc.service.FrameService;
 
@@ -27,5 +29,11 @@ public class FrameBlockController {
     val savedFrameBlockDto = frameBlockService.createFrameBlock(dto, frame);
 
     return new ResponseEntity<>(savedFrameBlockDto, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/frame_blocks")
+  public ResponseEntity<FrameBlockDto> updateFrameBlock(
+      @RequestBody @Valid UpdateFrameBlockDto dto) {
+    return new ResponseEntity<>(frameBlockService.updateFrameBlock(dto), HttpStatus.OK);
   }
 }
--- src/test/java/sidim/doma/wc/controller/FrameBlockControllerTest.java
+++ src/test/java/sidim/doma/wc/controller/FrameBlockControllerTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,11 +15,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import sidim.doma.wc.dto.frame_block.FrameBlockDto;
 import sidim.doma.wc.dto.frame_block.NewFrameBlockDto;
+import sidim.doma.wc.dto.frame_block.UpdateFrameBlockDto;
 import sidim.doma.wc.entity.Frame;
+import sidim.doma.wc.exception.FrameServiceException;
 import sidim.doma.wc.service.FrameBlockService;
 import sidim.doma.wc.service.FrameService;
 
@@ -40,18 +44,28 @@ class FrameBlockControllerTest {
   private ObjectMapper objectMapper;
 
   private final Integer frameId = 1;
-  private Integer blockId = 2;
+  private final Integer blockId = 2;
   private final String name = "test_block";
+  private final String updateName = "another_test_block";
   private final Boolean isWindowSizeEnabled = false;
   private final String blockInput = "test_block_input";
+  private final String updateBlockInput = "another_test_block_input";
   private Frame frame;
   private NewFrameBlockDto newFrameBlockDto;
+  private NewFrameBlockDto invalidNewFrameBlockDto;
+  private UpdateFrameBlockDto updateFrameBlockDto;
+  private UpdateFrameBlockDto invalidUpdateFrameBlockDto;
   private FrameBlockDto frameBlockDto;
+  private FrameBlockDto updatedFrameBlockDto;
 
   @BeforeEach
   void setUp() {
     newFrameBlockDto = new NewFrameBlockDto(frameId, name, isWindowSizeEnabled, blockInput, null);
+    invalidNewFrameBlockDto = new NewFrameBlockDto(frameId, null, isWindowSizeEnabled, blockInput, null);
     frameBlockDto = new FrameBlockDto(blockId, name, isWindowSizeEnabled, blockInput, null);
+    updatedFrameBlockDto = new FrameBlockDto(blockId, updateName, isWindowSizeEnabled, updateBlockInput, null);
+    updateFrameBlockDto = new UpdateFrameBlockDto(blockId, updateName, isWindowSizeEnabled, updateBlockInput, null);
+    invalidUpdateFrameBlockDto = new UpdateFrameBlockDto(blockId, null, isWindowSizeEnabled, updateBlockInput, null);
 
     frame = new Frame();
     frame.setId(frameId);
@@ -75,5 +89,62 @@ class FrameBlockControllerTest {
     assertEquals(blockInput, blockDto.inputTitle());
     assertEquals(isWindowSizeEnabled, blockDto.isWindowSizeEnabled());
     assertNull(blockDto.description());
+  }
+
+  @Test
+  void createFrameBlock_whenFrameNotFound() throws Exception {
+    when(frameService.getFrame(frameId)).thenThrow(
+        new FrameServiceException("Frame not found", HttpStatus.NOT_FOUND));
+
+    mockMvc.perform(post(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(newFrameBlockDto)))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  void createFrameBlock_whenInvalidDataProvided() throws Exception {
+    mockMvc.perform(post(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(invalidNewFrameBlockDto)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void updateFrameBlock_success() throws Exception {
+    when(frameBlockService.updateFrameBlock(updateFrameBlockDto)).thenReturn(updatedFrameBlockDto);
+
+    val mvcResult = mockMvc.perform(put(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(updateFrameBlockDto)))
+        .andExpect(status().isOk()).andReturn();
+
+    val content = mvcResult.getResponse().getContentAsString();
+    val blockDto = objectMapper.readValue(content, FrameBlockDto.class);
+
+    assertEquals(blockId, blockDto.id());
+    assertEquals(updateName, blockDto.name());
+    assertEquals(updateBlockInput, blockDto.inputTitle());
+    assertEquals(isWindowSizeEnabled, blockDto.isWindowSizeEnabled());
+    assertNull(blockDto.description());
+  }
+
+  @Test
+  void updateFrameBlock_whenInvalidDataProvided() throws Exception {
+    mockMvc.perform(put(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(invalidUpdateFrameBlockDto)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void updateFrameBlock_whenFrameBlockNotFound() throws Exception {
+    when(frameBlockService.updateFrameBlock(updateFrameBlockDto)).thenThrow(
+        new FrameServiceException("Frame block not found", HttpStatus.NOT_FOUND));
+
+    mockMvc.perform(put(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(updateFrameBlockDto)))
+        .andExpect(status().isNotFound());
   }
 }
