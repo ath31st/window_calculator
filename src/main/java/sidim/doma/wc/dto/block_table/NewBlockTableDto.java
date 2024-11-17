6b5a3b5b5eee688286badad97f9df545191285f5
ath31st <ath31st@yandex.ru>
2024-11-17 14:17:25 +0300

    add constraints for new and update dtos for block table entity

--- src/main/java/sidim/doma/wc/dto/block_table/NewBlockTableDto.java
+++ src/main/java/sidim/doma/wc/dto/block_table/NewBlockTableDto.java
@@ -1,11 +1,18 @@
 package sidim.doma.wc.dto.block_table;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
 import sidim.doma.wc.util.ButtonType;
 
 public record NewBlockTableDto(
+    @NotNull(message = "Frame block id is required and cannot be null")
     Integer frameBlockId,
+    @NotEmpty(message = "Name is required and cannot be empty")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
     String name,
+    @NotNull(message = "Button type is required and cannot be null")
     ButtonType buttonType
 ) implements Serializable {
 }
