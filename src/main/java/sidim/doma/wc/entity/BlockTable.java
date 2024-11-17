cbf38578193fefdd5b286153c36640b348d61413
ath31st <ath31st@yandex.ru>
2024-11-17 13:02:10 +0300

    update BlockTable with ButtonType

--- src/main/java/sidim/doma/wc/entity/BlockTable.java
+++ src/main/java/sidim/doma/wc/entity/BlockTable.java
@@ -3,6 +3,8 @@ package sidim.doma.wc.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,7 @@ import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import sidim.doma.wc.util.ButtonType;
 
 @Getter
 @Setter
@@ -44,8 +47,9 @@ public class BlockTable {
   @Column(name = "name")
   private String name;
 
+  @Enumerated(EnumType.STRING)
   @Column(name = "button_type")
-  private Integer buttonType;
+  private ButtonType buttonType;
 
   @ColumnDefault("CURRENT_TIMESTAMP")
   @Column(name = "created_at")
--- src/main/resources/db/migration/create_tables/V003__create_table_block_tables.sql
+++ src/main/resources/db/migration/create_tables/V003__create_table_block_tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE block_tables
     id             SERIAL PRIMARY KEY,
     frame_block_id INTEGER REFERENCES frame_blocks (id) ON DELETE CASCADE,
     name           VARCHAR(255) NOT NULL,
-    button_type    INTEGER      NOT NULL,
+    button_type    VARCHAR(255) NOT NULL,
     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
