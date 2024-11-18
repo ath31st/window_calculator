829cfd001dcc6216a4fe6c532e1223d943a757e7
ath31st <ath31st@yandex.ru>
2024-11-18 09:38:23 +0300

    add delete and update methods with tests for BlockTableController

--- src/main/java/sidim/doma/wc/controller/BlockTableController.java
+++ src/main/java/sidim/doma/wc/controller/BlockTableController.java
@@ -5,12 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sidim.doma.wc.dto.block_table.BlockTableDto;
 import sidim.doma.wc.dto.block_table.NewBlockTableDto;
+import sidim.doma.wc.dto.block_table.UpdateBlockTableDto;
 import sidim.doma.wc.service.BlockTableService;
 import sidim.doma.wc.service.FrameBlockService;
 
@@ -27,5 +31,19 @@ public class BlockTableController {
     val savedBlockTableDto = blockTableService.createNewBlockTable(dto, frameBlock);
 
     return new ResponseEntity<>(savedBlockTableDto, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/block_tables/{id}")
+  public ResponseEntity<HttpStatus> deleteBlockTable(@PathVariable Integer id) {
+    blockTableService.deleteBlockTable(id);
+
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @PutMapping("/block_tables")
+  public ResponseEntity<BlockTableDto> updateBlockTable(@RequestBody @Valid UpdateBlockTableDto dto) {
+    val blockTable = blockTableService.updateBlockTable(dto);
+
+    return new ResponseEntity<>(blockTable, HttpStatus.OK);
   }
 }
--- src/test/java/sidim/doma/wc/controller/BlockTableControllerTest.java
+++ src/test/java/sidim/doma/wc/controller/BlockTableControllerTest.java
@@ -1,13 +1,18 @@
 package sidim.doma.wc.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,9 +20,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.test.web.servlet.MockMvc;
 import sidim.doma.wc.dto.block_table.BlockTableDto;
 import sidim.doma.wc.dto.block_table.NewBlockTableDto;
+import sidim.doma.wc.dto.block_table.UpdateBlockTableDto;
 import sidim.doma.wc.entity.FrameBlock;
 import sidim.doma.wc.exception.FrameBlockServiceException;
 import sidim.doma.wc.service.BlockTableService;
@@ -116,5 +123,80 @@ class BlockTableControllerTest {
         .contentType("application/json")
         .content(objectMapper.writeValueAsString(newBlockTable))
     ).andExpect(status().isNotFound()).andReturn();
+  }
+
+  @Test
+  void deleteBlockTable_success() throws Exception {
+    mockMvc.perform(delete(BASE_URL + "/{id}", blockTableId))
+        .andExpect(status().isNoContent()).andReturn();
+  }
+
+  @Test
+  void deleteBlockTable_whenBlockTableNotFound() throws Exception {
+    doThrow(new FrameBlockServiceException("Block table not found", HttpStatus.NOT_FOUND))
+        .when(blockTableService).deleteBlockTable(blockTableId);
+
+    mockMvc.perform(delete(BASE_URL + "/{id}", blockTableId))
+        .andExpect(status().isNotFound()).andReturn();
+  }
+
+  @Test
+  void updateBlockTable_success() throws Exception {
+    val newName = "new_block_table_name";
+    val updateBlockTableDto = new UpdateBlockTableDto(blockTableId, newName, ButtonType.MODIFIER);
+
+    when(blockTableService.updateBlockTable(updateBlockTableDto))
+        .thenReturn(new BlockTableDto(blockTableId, newName, ButtonType.MODIFIER));
+
+    val result = mockMvc.perform(put(BASE_URL)
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(updateBlockTableDto)))
+        .andExpect(status().isOk()).andReturn();
+
+    val content = result.getResponse().getContentAsString();
+    val blockTableDto = objectMapper.readValue(content, BlockTableDto.class);
+
+    assertEquals(blockTableId, blockTableDto.id());
+    assertEquals(newName, blockTableDto.name());
+    assertEquals(ButtonType.MODIFIER, blockTableDto.buttonType());
+  }
+
+  @Test
+  void updateBlockTable_whenBlockTableNotFound() throws Exception {
+    doThrow(new FrameBlockServiceException("Block table not found", HttpStatus.NOT_FOUND))
+        .when(blockTableService).updateBlockTable(any(UpdateBlockTableDto.class));
+
+    mockMvc.perform(put(BASE_URL)
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(
+            new UpdateBlockTableDto(blockTableId, name, ButtonType.VALUE)))
+    ).andExpect(status().isNotFound()).andReturn();
+  }
+
+  @Test
+  void updateBlockTable_whenInvalidDataProvided_1() throws Exception {
+    mockMvc.perform(put(BASE_URL)
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(
+            new UpdateBlockTableDto(blockTableId, null, null)))
+    ).andExpect(status().isBadRequest()).andReturn();
+  }
+
+  @Test
+  void updateBlockTable_whenInvalidDataProvided_2() throws Exception {
+    mockMvc.perform(put(BASE_URL)
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(
+                Map.of(
+                    "blockTableId", blockTableId,
+                    "name", "new_name",
+                    "buttonType", "INVALID_TYPE"
+                ))
+            )
+        ).andExpect(status().isBadRequest())
+        .andExpect(result ->
+            assertInstanceOf(HttpMessageNotReadableException.class, result.getResolvedException())
+        )
+        .andReturn();
   }
 }
