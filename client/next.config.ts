a504744ca56b073cd310bcc6fee397472154c2c3
ath31st <ath31st@yandex.ru>
2024-11-23 13:43:17 +0300

    more config with sass styles

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   sassOptions: {
     implementation: 'sass-embedded',
-    additionalData: `
-      $primary-color: #0070f3;
-      $secondary-color: #ff6347;
-      $font-primary: 'Arial', sans-serif;
-      $font-secondary: 'Georgia', serif;
-      $base-font-size: 16px;
-    `,
   },
 };
 
