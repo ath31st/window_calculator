fd24a3a01b54324817f1c00c72346e440a53e9b4
ath31st <ath31st@yandex.ru>
2024-11-23 11:07:00 +0300

    init client side project

---
+++ client/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Simple Next.js Page</h1>
+      <p>This is the simplest page you can create in Next.js.</p>
+    </div>
+  );
+}
