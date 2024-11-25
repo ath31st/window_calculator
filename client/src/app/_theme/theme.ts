1b06e72e599ad85f01307d1723399a07052b52e6
ath31st <ath31st@yandex.ru>
2024-11-25 21:37:06 +0300

    add breakpoints for page width

--- client/src/app/_theme/theme.ts
+++ client/src/app/_theme/theme.ts
@@ -3,6 +3,15 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   cssVariables: true,
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+    },
+  },
   typography: {
     fontFamily: 'var(--font-roboto)',
   },
