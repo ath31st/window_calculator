5bb57bf8a7928a74fcdd26de8d5bdaa8e8df9e60
ath31st <ath31st@yandex.ru>
2024-11-26 11:45:16 +0300

    create frame service with functions

---
+++ client/src/services/frame.service.ts
@@ -0,0 +1,33 @@
+import { Frame, FrameFull } from '@/types/api';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL + '/frames';
+
+export const fetchFrames = async (): Promise<Frame[]> => {
+  const response = await axios.get<Frame[]>(API_URL);
+  return response.data;
+};
+
+export const fetchFrame = async (id: number): Promise<Frame> => {
+  const response = await axios.get<Frame>(`${API_URL}/${id}`);
+  return response.data;
+};
+
+export const fetchFrameFull = async (id: number): Promise<FrameFull> => {
+  const response = await axios.get<FrameFull>(`${API_URL}/${id}/full`);
+  return response.data;
+};
+
+export const addFrame = async (name: string): Promise<Frame> => {
+  const response = await axios.post<Frame>(API_URL, { name });
+  return response.data;
+};
+
+export const deleteFrame = async (id: number): Promise<void> => {
+  await axios.delete(`${API_URL}/${id}`);
+};
+
+export const updateFrame = async (id: number, name: string): Promise<Frame> => {
+  const response = await axios.put<Frame>(`${API_URL}/${id}`, { name });
+  return response.data;
+};
