5e83dc92ad5f8c526f682daf59c2d8a54a7ad0df
ath31st <ath31st@yandex.ru>
2024-11-27 11:31:50 +0300

    change color set

--- client/src/app/_theme/theme.ts
+++ client/src/app/_theme/theme.ts
@@ -1,62 +1,53 @@
 'use client';
 import { createTheme } from '@mui/material/styles';
 
-const getCssVariable = (name: string) => {
-  if (typeof window !== 'undefined') {
-    return getComputedStyle(document.documentElement)
-      .getPropertyValue(name)
-      .trim();
-  }
-  return '';
-};
-
 const theme = createTheme({
   palette: {
     primary: {
-      main: getCssVariable('--color-primary') || '#8b4513',
+      main: '#0078d7',
     },
     secondary: {
-      main: getCssVariable('--color-secondary') || '#d2b48c',
+      main: '#b0c4de',
     },
     background: {
-      default: getCssVariable('--color-bg-base') || '#3e2723',
-      paper: getCssVariable('--color-bg-container') || '#795548',
+      default: '#f5f5f5',
+      paper: '#e0e0e0',
     },
     text: {
-      primary: getCssVariable('--color-text-base') || '#f5f5dc',
-      secondary: getCssVariable('--color-text-secondary') || '#d7ccc8',
+      primary: '#333333',
+      secondary: '#666666',
     },
     success: {
-      main: getCssVariable('--color-success') || '#27ae60',
+      main: '#27ae60',
     },
     warning: {
-      main: getCssVariable('--color-warning') || '#e67e22',
+      main: '#e67e22',
     },
     error: {
-      main: getCssVariable('--color-error') || '#c0392b',
+      main: '#c0392b',
     },
     info: {
-      main: getCssVariable('--color-info') || '#2980b9',
+      main: '#2980b9',
     },
   },
   shape: {
-    borderRadius: parseInt(getCssVariable('--border-radius') || '0', 10),
+    borderRadius: 3,
   },
   typography: {
-    fontSize: parseInt(getCssVariable('--font-size') || '18', 10),
+    fontSize: 18,
   },
   transitions: {
     duration: {
-      standard: parseInt(getCssVariable('--transition-duration') || '300', 10),
+      standard: 300,
     },
   },
   breakpoints: {
     values: {
       xs: 0,
-      sm: parseInt(getCssVariable('--breakpoint-sm') || '576', 10),
-      md: parseInt(getCssVariable('--breakpoint-md') || '768', 10),
-      lg: parseInt(getCssVariable('--breakpoint-lg') || '992', 10),
-      xl: parseInt(getCssVariable('--breakpoint-xl') || '1200', 10),
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
     },
   },
 });
