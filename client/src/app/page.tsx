4bb9a19a28c00c8129e01ad8279ef18b65301561
ath31st <ath31st@yandex.ru>
2024-11-27 14:05:04 +0300

    working on main page v1

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,38 @@
-export default function Home() {
+'use client';
+
+import { Box, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import { useFrameBlockStore } from '@/stores/frame.block.store';
+import FrameBlockList from '@/components/FrameBlockList';
+import AddFrameBlockDialog from '@/components/dialogs/AddFrameBlockDialog';
+import { useFrameStore } from '@/stores/frame.store';
+
+const Home: React.FC = () => {
+  const { addFrameBlock, frameBlocksFull, deleteFrameBlock, updateFrameBlock } =
+    useFrameBlockStore();
+  const { activeFrameId } = useFrameStore();
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
   return (
-    <div>
-      <h1>Welcome to My Simple Next.js Page</h1>
-      <p>This is the simplest page you can create in Next.js.</p>
-    </div>
+    <Box sx={{ padding: 2 }}>
+      <FrameBlockList
+        frameBlocksFull={frameBlocksFull}
+        deleteFrameBlock={(id) => deleteFrameBlock(id)}
+        updateFrameBlock={(block) => updateFrameBlock(block)}
+      />
+      <IconButton onClick={() => setDialogOpen(true)}>
+        <AddIcon />
+      </IconButton>
+
+      <AddFrameBlockDialog
+        frameId={activeFrameId || 0}
+        isOpen={isDialogOpen}
+        onClose={() => setDialogOpen(false)}
+        onAdd={(newFrameBlock) => addFrameBlock(newFrameBlock)}
+      />
+    </Box>
   );
-}
+};
+
+export default Home;
