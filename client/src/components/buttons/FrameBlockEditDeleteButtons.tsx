e08b91b34e8f16b03fab689156a7c14afcd64f37
ath31st <ath31st@yandex.ru>
2024-11-28 10:17:12 +0300

    add edit/delete buttons component for frame block

---
+++ client/src/components/buttons/FrameBlockEditDeleteButtons.tsx
@@ -0,0 +1,51 @@
+import { FrameBlock } from '@/types/api';
+import { Box, IconButton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from 'react';
+import DeleteFrameBlockDialog from '../dialogs/frame.block/DeleteFrameBlockDialog';
+import EditFrameBlockDialog from '../dialogs/frame.block/EditFrameBlockDialog';
+
+interface FrameBlockEditDeleteButtonsProps {
+  id: number;
+  currentFrameBlock: FrameBlock;
+  onEdit: (block: FrameBlock) => void;
+  onDelete: (id: number) => void;
+}
+
+export const FrameBlockEditDeleteButtons: React.FC<
+  FrameBlockEditDeleteButtonsProps
+> = ({ id, currentFrameBlock, onEdit, onDelete }) => {
+  const [dialogType, setDialogType] = useState<'edit' | 'delete' | null>(null);
+
+  const openEditDialog = () => setDialogType('edit');
+  const openDeleteDialog = () => setDialogType('delete');
+  const closeDialog = () => setDialogType(null);
+
+  return (
+    <Box>
+      <Box sx={{ display: 'flex', gap: 0.5 }}>
+        <IconButton size="small" onClick={openEditDialog}>
+          <EditIcon fontSize="small" />
+        </IconButton>
+        <IconButton size="small" onClick={openDeleteDialog}>
+          <DeleteIcon fontSize="small" />
+        </IconButton>
+      </Box>
+
+      <EditFrameBlockDialog
+        isOpen={dialogType === 'edit'}
+        onClose={closeDialog}
+        onUpdate={onEdit}
+        currentFrameBlock={currentFrameBlock}
+      />
+
+      <DeleteFrameBlockDialog
+        isOpen={dialogType === 'delete'}
+        onClose={closeDialog}
+        onDelete={onDelete}
+        frameBlockId={id}
+      />
+    </Box>
+  );
+};
