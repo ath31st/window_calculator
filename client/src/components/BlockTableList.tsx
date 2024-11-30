d6144b7978b74815953d9314497d2bc7771bc4d9
ath31st <ath31st@yandex.ru>
2024-11-30 18:28:47 +0300

    add block table list component

---
+++ client/src/components/BlockTableList.tsx
@@ -0,0 +1,38 @@
+import { Box } from '@mui/material';
+import { BlockTable, BlockTableFull } from '@/types/api';
+import BlockTableCard from './BlockTableCard';
+
+interface BlockTableListProps {
+  blockTablesFull: BlockTableFull[];
+  deleteBlockTable: (id: number) => void;
+  updateBlockTable: (table: BlockTable) => void;
+}
+
+const BlockTableList: React.FC<BlockTableListProps> = ({
+  blockTablesFull,
+  deleteBlockTable,
+  updateBlockTable,
+}) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        padding: 2,
+        overflowY: 'auto',
+      }}
+    >
+      {blockTablesFull.map((table) => (
+        <BlockTableCard
+          key={table.id}
+          table={table}
+          onEdit={updateBlockTable}
+          onDelete={deleteBlockTable}
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default BlockTableList;
