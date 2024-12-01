ecec9a5eb9ffe17ef66377be4d19bf1f23f464a9
ath31st <ath31st@yandex.ru>
2024-12-01 16:39:18 +0300

    add component with edit/delete functions table button

---
+++ client/src/components/buttons/TableButtonEditDeleteButtons.tsx
@@ -0,0 +1,52 @@
+import { TableButton } from '@/types/api';
+import { Box, IconButton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from 'react';
+import EditTableButtonDialog from '../dialogs/table.button/EditTableButtonDialog';
+import DeleteTableButtonDialog from '../dialogs/table.button/DeleteTableButtonDialog';
+
+interface TableButtonEditDeleteButtonsProps {
+  currentTableButton: TableButton;
+  onEdit: (button: TableButton) => void;
+  onDelete: (id: number) => void;
+}
+
+const TableButtonEditDeleteButtons: React.FC<
+  TableButtonEditDeleteButtonsProps
+> = ({ currentTableButton, onEdit, onDelete }) => {
+  const [dialogType, setDialogType] = useState<'edit' | 'delete' | null>(null);
+
+  const openEditDialog = () => setDialogType('edit');
+  const openDeleteDialog = () => setDialogType('delete');
+  const closeDialog = () => setDialogType(null);
+
+  return (
+    <Box>
+      <Box sx={{ display: 'flex', gap: 0.5 }}>
+        <IconButton size="small" onClick={openEditDialog}>
+          <EditIcon fontSize="small" />
+        </IconButton>
+        <IconButton size="small" onClick={openDeleteDialog}>
+          <DeleteIcon fontSize="small" />
+        </IconButton>
+      </Box>
+
+      <EditTableButtonDialog
+        isOpen={dialogType === 'edit'}
+        onClose={closeDialog}
+        onUpdate={onEdit}
+        currentTableButton={currentTableButton}
+      />
+
+      <DeleteTableButtonDialog
+        isOpen={dialogType === 'delete'}
+        onClose={closeDialog}
+        onDelete={onDelete}
+        tableButtonId={currentTableButton.id}
+      />
+    </Box>
+  );
+};
+
+export default TableButtonEditDeleteButtons;
