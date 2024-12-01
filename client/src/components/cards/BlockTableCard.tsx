6d3a60ff6cc16f5818e8cdd0bd1754c0e7552f49
ath31st <ath31st@yandex.ru>
2024-12-01 17:05:37 +0300

    update block table card with table buttons logic

--- client/src/components/cards/BlockTableCard.tsx
+++ client/src/components/cards/BlockTableCard.tsx
@@ -1,6 +1,11 @@
 import { BlockTable, BlockTableFull } from '@/types/api';
-import { Box, Card, CardContent, Typography } from '@mui/material';
+import { Box, Card, CardContent, IconButton, Typography } from '@mui/material';
 import { BlockTableEditDeleteButtons } from '../buttons/BlockTableEditDeleteButtons';
+import { useState } from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import AddTableButtonDialog from '../dialogs/table.button/AddTableButtonDialog';
+import TableButtonList from '../lists/TableButtonList';
+import { useTableButtonStore } from '@/stores/table.button.store';
 
 interface BlockTableCardProps {
   table: BlockTableFull;
@@ -13,6 +18,10 @@ const BlockTableCard: React.FC<BlockTableCardProps> = ({
   onEdit,
   onDelete,
 }) => {
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const { addTableButton, updateTableButton, deleteTableButton } =
+    useTableButtonStore();
+
   return (
     <Card sx={{ width: '100%', mb: 2, position: 'relative' }}>
       <CardContent>
@@ -21,9 +30,29 @@ const BlockTableCard: React.FC<BlockTableCardProps> = ({
             {table.name}
           </Typography>
 
+          <TableButtonList
+            tableButtons={table.tableButtons}
+            updateTableButton={(tableButton) => updateTableButton(tableButton)}
+            deleteTableButton={(id) => deleteTableButton(id)}
+            buttonType={table.buttonType}
+          />
+
           <Typography variant="body2" sx={{ mb: 2 }}>
             {table.buttonType}
           </Typography>
+
+          <Box>
+            <IconButton onClick={() => setDialogOpen(true)}>
+              <AddIcon />
+            </IconButton>
+          </Box>
+
+          <AddTableButtonDialog
+            isOpen={isDialogOpen}
+            onClose={() => setDialogOpen(false)}
+            onAdd={(tableButton) => addTableButton(tableButton)}
+            blockTableId={table.id}
+          />
 
           <Box
             sx={{
