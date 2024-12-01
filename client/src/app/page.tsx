26527d3f75861e8a60a268e7133c9c6bf4ea3a72
ath31st <ath31st@yandex.ru>
2024-12-01 11:56:56 +0300

    add frame card component, rework main page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,37 +1,21 @@
 'use client';
 
-import { Box, IconButton } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { useState } from 'react';
 import { useFrameBlockStore } from '@/stores/frame.block.store';
-import FrameBlockList from '@/components/lists/FrameBlockList';
-import AddFrameBlockDialog from '@/components/dialogs/frame.block/AddFrameBlockDialog';
 import { useFrameStore } from '@/stores/frame.store';
+import FrameCard from '@/components/cards/FrameCard';
 
 const Home: React.FC = () => {
   const { addFrameBlock, frameBlocksFull, deleteFrameBlock, updateFrameBlock } =
     useFrameBlockStore();
   const { activeFrameId } = useFrameStore();
-  const [isDialogOpen, setDialogOpen] = useState(false);
-
   return (
-    <Box sx={{ padding: 2 }}>
-      <FrameBlockList
-        frameBlocksFull={frameBlocksFull}
-        deleteFrameBlock={(id) => deleteFrameBlock(id)}
-        updateFrameBlock={(block) => updateFrameBlock(block)}
-      />
-      <IconButton onClick={() => setDialogOpen(true)}>
-        <AddIcon />
-      </IconButton>
-
-      <AddFrameBlockDialog
-        frameId={activeFrameId || 0}
-        isOpen={isDialogOpen}
-        onClose={() => setDialogOpen(false)}
-        onAdd={(newFrameBlock) => addFrameBlock(newFrameBlock)}
-      />
-    </Box>
+    <FrameCard
+      activeFrameId={activeFrameId}
+      frameBlocksFull={frameBlocksFull}
+      onAdd={(newFrameBlock) => addFrameBlock(newFrameBlock)}
+      onDelete={(id) => deleteFrameBlock(id)}
+      onEdit={(block) => updateFrameBlock(block)}
+    />
   );
 };
 
---
+++ client/src/components/cards/FrameCard.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Box, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import FrameBlockList from '@/components/lists/FrameBlockList';
+import AddFrameBlockDialog from '@/components/dialogs/frame.block/AddFrameBlockDialog';
+import { FrameBlock, FrameBlockFull, NewFrameBlock } from '@/types/api';
+
+interface FrameCardProps {
+  activeFrameId: number | null;
+  frameBlocksFull: FrameBlockFull[];
+  onAdd: (frameBlock: NewFrameBlock) => void;
+  onDelete: (id: number) => void;
+  onEdit: (block: FrameBlock) => void;
+}
+
+const FrameCard: React.FC<FrameCardProps> = ({
+  activeFrameId,
+  frameBlocksFull,
+  onAdd,
+  onDelete,
+  onEdit,
+}) => {
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  return (
+    <Box sx={{ padding: 2 }}>
+      <FrameBlockList
+        frameBlocksFull={frameBlocksFull}
+        deleteFrameBlock={onDelete}
+        updateFrameBlock={onEdit}
+      />
+      <IconButton onClick={() => setDialogOpen(true)}>
+        <AddIcon />
+      </IconButton>
+
+      <AddFrameBlockDialog
+        frameId={activeFrameId || 0}
+        isOpen={isDialogOpen}
+        onClose={() => setDialogOpen(false)}
+        onAdd={onAdd}
+      />
+    </Box>
+  );
+};
+
+export default FrameCard;
