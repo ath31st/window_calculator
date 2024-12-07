f3f04b6131f4f5c6b1c14590ec25a58626dfa2b8
ath31st <ath31st@yandex.ru>
2024-12-07 10:21:42 +0300

    rework icon logic in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,22 +3,42 @@
 import { Box, IconButton, Badge } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import HomeIcon from '@mui/icons-material/Home';
 import { useCartStore } from '@/stores/cart.store';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Header: React.FC = () => {
   const { cartItems } = useCartStore();
+  const pathname = usePathname();
 
   const handleUserClick = () => {
     console.log('User icon clicked');
   };
+
+  const routes = [
+    {
+      path: '/',
+      icon: <HomeIcon />,
+      showOn: ['/cart', '/profile'],
+    },
+    {
+      path: '/cart',
+      icon: (
+        <Badge badgeContent={cartItems.length} color="primary">
+          <ShoppingCartIcon />
+        </Badge>
+      ),
+      showOn: ['/'],
+    },
+  ];
 
   return (
     <Box
       sx={{
         position: 'fixed',
         top: 16,
-        right: 16,
+        right: 8,
         display: 'flex',
         flexDirection: 'column',
         gap: 1,
@@ -32,13 +52,16 @@ const Header: React.FC = () => {
         <AccountCircleIcon />
       </IconButton>
 
-      <Link href="/cart" passHref>
-        <IconButton sx={{ bgcolor: 'background.paper', boxShadow: 2 }}>
-          <Badge badgeContent={cartItems.length} color="primary">
-            <ShoppingCartIcon />
-          </Badge>
-        </IconButton>
-      </Link>
+      {routes.map(
+        ({ path, icon, showOn }) =>
+          showOn.includes(pathname) && (
+            <Link key={path} href={path} passHref>
+              <IconButton sx={{ bgcolor: 'background.paper', boxShadow: 2 }}>
+                {icon}
+              </IconButton>
+            </Link>
+          ),
+      )}
     </Box>
   );
 };
