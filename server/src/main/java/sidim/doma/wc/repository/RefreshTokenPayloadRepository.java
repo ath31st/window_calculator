fb02f8a7243c88555dcd6704fdcbc1ca36f4559b
ath31st <ath31st@yandex.ru>
2024-12-10 13:21:22 +0300

    add RefreshTokenPayloadRepository

--- server/src/main/java/sidim/doma/wc/repository/RefreshTokenPayloadRepository.java
+++ server/src/main/java/sidim/doma/wc/repository/RefreshTokenPayloadRepository.java
@@ -1,6 +1,7 @@
 package sidim.doma.wc.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,12 +12,12 @@ import sidim.doma.wc.entity.RefreshTokenPayload;
 @Repository
 public interface RefreshTokenPayloadRepository extends JpaRepository<RefreshTokenPayload, Long> {
 
-  boolean existsByUserEmailIgnoreCase(String username);
+  boolean existsByUserEmailIgnoreCase(String email);
 
   @Transactional
   @Modifying
   @Query("update RefreshTokenPayload p set p.payload = ?1 where upper(p.user.email) = upper(?2)")
-  void updatePayloadByEmailIgnoreCase(String payload, String email);
+  void updatePayloadByEmailIgnoreCase(UUID payload, String email);
 
-  Optional<RefreshTokenPayload> findByUserEmailIgnoreCase(String username);
+  Optional<RefreshTokenPayload> findByUserEmailIgnoreCase(String email);
 }
