a13be2795f6a724ce18428ce67570c2448c55d9f
ath31st <ath31st@yandex.ru>
2024-12-13 12:31:37 +0300

    rework auth methods in service

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,16 +1,20 @@
-import { Credentials } from '@/types/api';
+import { Credentials, JwtResponse } from '@/types/api';
 import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL + '/auth';
 
-export const login = async (credentials: Credentials): Promise<void> => {
-  await axios.post(`${API_URL}/login`, credentials);
+export const login = async (credentials: Credentials): Promise<JwtResponse> => {
+  const response = await axios.post(`${API_URL}/login`, credentials);
+  return response.data;
 };
 
 export const logout = async (): Promise<void> => {
-  await axios.post(`${API_URL}/logout`);
+  await axios.delete(`${API_URL}/logout`);
 };
 
-export const refreshToken = async (refreshToken: string): Promise<void> => {
-  await axios.post(`${API_URL}/refresh`, { refreshToken });
+export const refreshAccessToken = async (
+  refreshToken: string,
+): Promise<JwtResponse> => {
+  const response = await axios.post(`${API_URL}/refresh`, { refreshToken });
+  return response.data;
 };
