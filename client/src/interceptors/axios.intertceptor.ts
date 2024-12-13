531523aafcb7e9ccd26cb25b6d1c1d37a7d7fdc4
ath31st <ath31st@yandex.ru>
2024-12-13 13:29:56 +0300

    add axios interceptor

---
+++ client/src/interceptors/axios.intertceptor.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { LOCAL_STORAGE_KEY } from '@/constants/auth';
+import { useAuthStore } from '@/stores/auth.store';
+import { isTokenExpired } from '@/services/jwt.service';
+
+const setupAxiosInterceptor = (setServerStatus: (status: boolean) => void) => {
+  axios.interceptors.response.use(
+    (response) => {
+      setServerStatus(true);
+      return response;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+      const { status } = error.response;
+
+      if (status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        const refreshToken = useAuthStore.getState().refreshToken;
+        if (refreshToken && !isTokenExpired(refreshToken)) {
+          try {
+            await useAuthStore.getState().refreshAccessToken();
+            const newAccessToken = useAuthStore.getState().accessToken;
+            originalRequest.headers['Authorization'] =
+              `Bearer ${newAccessToken}`;
+            return await axios(originalRequest);
+          } catch (refreshError) {
+            console.error('Error refreshing access token:', refreshError);
+          }
+        }
+        await useAuthStore.getState().logout();
+        return Promise.reject(error);
+      }
+
+      if (!error.response) {
+        setServerStatus(false);
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  axios.interceptors.request.use(
+    (config) => {
+      const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+      if (storedData) {
+        const { accessToken } = JSON.parse(storedData);
+        if (accessToken) {
+          config.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+};
+
+export default setupAxiosInterceptor;
