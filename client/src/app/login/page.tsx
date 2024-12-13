85252ac2e3ac4f1b10832556a2ef16138fe2eaf5
ath31st <ath31st@yandex.ru>
2024-12-13 13:24:57 +0300

    update login page with auth logic

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,11 +1,58 @@
-import LoginForm from '@/components/LoginForm';
+'use client';
 
-const LoginPage = () => {
+import CommonLayout from '@/components/layouts/CommonLayout';
+import LoginForm from '@/components/LoginForm';
+import React from 'react';
+import { useAuthStore } from '@/stores/auth.store';
+import { Box, CircularProgress, Typography } from '@mui/material';
+import { useRouter } from 'next/navigation';
+
+const LoginPage: React.FC = () => {
+  const { login, isLoading, error, clearError } = useAuthStore();
+  const router = useRouter();
+
+  const handleLogin = async (credentials: {
+    email: string;
+    password: string;
+  }) => {
+    try {
+      console.log('Logging in...');
+      await login(credentials);
+      router.push('/');
+    } catch {
+      console.error('Failed to log in');
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <LoginForm />
-    </div>
+    <CommonLayout>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          gap: 3,
+        }}
+      >
+        <Typography variant="h4">Вход</Typography>
+        {error && (
+          <Typography
+            color="error"
+            onClick={clearError}
+            sx={{ cursor: 'pointer' }}
+          >
+            {error}
+          </Typography>
+        )}
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <LoginForm onSubmit={handleLogin} />
+        )}
+      </Box>
+    </CommonLayout>
   );
 };
 
