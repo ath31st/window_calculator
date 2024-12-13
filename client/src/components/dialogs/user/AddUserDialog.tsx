eb41d43faf179b53e02d1b9f44f62d3401966a29
ath31st <ath31st@yandex.ru>
2024-12-13 10:50:04 +0300

    create add user dialog

---
+++ client/src/components/dialogs/user/AddUserDialog.tsx
@@ -0,0 +1,83 @@
+import { NewUser } from '@/types/api';
+import {
+  Box,
+  Button,
+  Dialog,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+
+interface AddUserDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onAdd: (user: NewUser) => void;
+}
+
+const AddUserDialog: React.FC<AddUserDialogProps> = ({
+  isOpen,
+  onClose,
+  onAdd,
+}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState(2);
+
+  const handleAdd = () => {
+    if (email.trim() && password.trim()) {
+      const newUser: NewUser = {
+        email: email.trim(),
+        password: password.trim(),
+        role: role,
+      };
+      onAdd(newUser);
+      setEmail('');
+      setPassword('');
+      setRole(0);
+      onClose();
+    }
+  };
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <Box
+        sx={{
+          padding: 3,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          minWidth: 300,
+        }}
+      >
+        <Typography variant="h6">Добавить пользователя</Typography>
+        <TextField
+          label="Email пользователя"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          fullWidth
+        />
+        <TextField
+          label="Введите пароль для пользователя"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+        />
+        <Select
+          value={role}
+          onChange={(e) => setRole(Number(e.target.value))}
+          fullWidth
+          displayEmpty
+        >
+          <MenuItem value={2}>Пользователь</MenuItem>
+          <MenuItem value={1}>Администратор</MenuItem>
+        </Select>
+        <Button variant="contained" onClick={handleAdd}>
+          Добавить
+        </Button>
+      </Box>
+    </Dialog>
+  );
+};
+
+export default AddUserDialog;
