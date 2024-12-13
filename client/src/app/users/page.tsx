36870d03809cbc455b10ae97aa1271b5696eb45f
ath31st <ath31st@yandex.ru>
2024-12-13 11:33:15 +0300

    add users page

---
+++ client/src/app/users/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import AddIcon from '@mui/icons-material/Add';
+import Header from '@/components/Header';
+import CommonLayout from '@/components/layouts/CommonLayout';
+import { useUserStore } from '@/stores/user.store';
+import { useState } from 'react';
+import AddUserDialog from '@/components/dialogs/user/AddUserDialog';
+import UserList from '@/components/lists/UserList';
+import { IconButton } from '@mui/material';
+
+const Users: React.FC = () => {
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const { addUser, updateUser, deleteUser, users } = useUserStore();
+
+  return (
+    <CommonLayout>
+      <Header />
+      <h1>Users</h1>
+
+      <UserList users={users} onEdit={updateUser} onDelete={deleteUser} />
+
+      <IconButton onClick={() => setDialogOpen(true)}>
+        <AddIcon />
+      </IconButton>
+
+      <AddUserDialog
+        isOpen={isDialogOpen}
+        onClose={() => setDialogOpen(false)}
+        onAdd={addUser}
+      />
+    </CommonLayout>
+  );
+};
+
+export default Users;
