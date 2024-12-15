7d0a5270fe4260dd2c6b6103465ebb77b273ad1f
ath31st <ath31st@yandex.ru>
2024-12-15 16:55:50 +0300

    fix absent annotation in RefreshTokenPayload

--- server/src/main/java/sidim/doma/wc/entity/RefreshTokenPayload.java
+++ server/src/main/java/sidim/doma/wc/entity/RefreshTokenPayload.java
@@ -3,6 +3,8 @@ package sidim.doma.wc.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -28,6 +30,7 @@ import org.hibernate.annotations.OnDeleteAction;
 @Table(name = "refresh_token_payloads", schema = "win_calc_schema")
 public class RefreshTokenPayload {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @ColumnDefault("nextval('win_calc_schema.refresh_token_payloads_id_seq')")
   @Column(name = "id", nullable = false)
   private Long id;
