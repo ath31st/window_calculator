de4a7ecb2286bd6797191b0fdf233f491516ea20
ath31st <ath31st@yandex.ru>
2024-12-15 16:13:31 +0300

    add auth provider

---
+++ client/src/providers/AuthProvider.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useAuthStore } from '@/stores/auth.store';
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const { hydrate } = useAuthStore();
+
+  useEffect(() => {
+    hydrate();
+  }, [hydrate]);
+
+  if (useAuthStore.getState().isLoading) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default AuthProvider;
