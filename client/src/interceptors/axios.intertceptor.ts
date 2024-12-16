a3de970c07fa2b0a681962955fd03a93c36f981a
ath31st <ath31st@yandex.ru>
2024-12-16 09:31:35 +0300

    rework axios interceptor

--- client/src/interceptors/axios.intertceptor.ts
+++ client/src/interceptors/axios.intertceptor.ts
@@ -3,38 +3,19 @@ import { LOCAL_STORAGE_KEY } from '@/constants/auth';
 import { useAuthStore } from '@/stores/auth.store';
 import { isTokenExpired } from '@/services/jwt.service';
 
+let isRefreshing = false;
+let refreshSubscribers: ((token: string) => void)[] = [];
+
+const subscribeTokenRefresh = (callback: (token: string) => void) => {
+  refreshSubscribers.push(callback);
+};
+
+const onRrefreshed = (newAccessToken: string) => {
+  refreshSubscribers.forEach((callback) => callback(newAccessToken));
+  refreshSubscribers = [];
+};
+
 const setupAxiosInterceptor = () => {
-  axios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    async (error) => {
-      const originalRequest = error.config;
-      const { status } = error.response;
-
-      if (status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-
-        const refreshToken = useAuthStore.getState().refreshToken;
-        if (refreshToken && !isTokenExpired(refreshToken)) {
-          try {
-            await useAuthStore.getState().refreshAccessToken();
-            const newAccessToken = useAuthStore.getState().accessToken;
-            originalRequest.headers['Authorization'] =
-              `Bearer ${newAccessToken}`;
-            return await axios(originalRequest);
-          } catch (refreshError) {
-            console.error('Error refreshing access token:', refreshError);
-          }
-        }
-        await useAuthStore.getState().logout();
-        return Promise.reject(error);
-      }
-
-      return Promise.reject(error);
-    },
-  );
-
   axios.interceptors.request.use(
     (config) => {
       const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
@@ -47,6 +28,61 @@ const setupAxiosInterceptor = () => {
       return config;
     },
     (error) => {
+      return Promise.reject(error);
+    },
+  );
+
+  axios.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+      const { status } = error.response;
+
+      if (status === 401 && !originalRequest._retry) {
+        const refreshToken = useAuthStore.getState().refreshToken;
+
+        if (!refreshToken || isTokenExpired(refreshToken)) {
+          await useAuthStore.getState().logout();
+          return Promise.reject(error);
+        }
+
+        if (isRefreshing) {
+          return new Promise((resolve, reject) => {
+            subscribeTokenRefresh((newToken) => {
+              if (newToken) {
+                originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+                resolve(axios(originalRequest));
+              } else {
+                reject(error);
+              }
+            });
+          });
+        }
+
+        originalRequest._retry = true;
+        isRefreshing = true;
+
+        try {
+          await useAuthStore.getState().refreshAccessToken();
+          const newAccessToken = useAuthStore.getState().accessToken;
+
+          if (newAccessToken) {
+            onRrefreshed(newAccessToken);
+            originalRequest.headers['Authorization'] =
+              `Bearer ${newAccessToken}`;
+            return axios(originalRequest);
+          } else {
+            throw new Error('New access token is null');
+          }
+        } catch (refreshError) {
+          console.error('Error refreshing access token:', refreshError);
+          await useAuthStore.getState().logout();
+          return Promise.reject(refreshError);
+        } finally {
+          isRefreshing = false;
+        }
+      }
+
       return Promise.reject(error);
     },
   );
