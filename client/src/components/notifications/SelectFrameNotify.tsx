0307ddd024a348f87584d036aacee4c294d9c83b
ath31st <ath31st@yandex.ru>
2024-12-21 09:15:28 +0300

    update notify for select frame

--- client/src/components/notifications/SelectFrameNotify.tsx
+++ client/src/components/notifications/SelectFrameNotify.tsx
@@ -1,16 +1,38 @@
-import { Box } from '@mui/material';
+'use client';
+
+import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
 
 const SelectFrameNotify: React.FC = () => {
   return (
     <Box
       sx={{
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh',
+        height: '90vh',
+        textAlign: 'center',
       }}
     >
-      Select a frame
+      <Typography variant="h5" sx={{ mb: 4 }}>
+        Выберите фрейм
+      </Typography>
+      <Box
+        sx={{
+          position: 'relative',
+          width: '100px',
+          height: '100px',
+        }}
+      >
+        <Image
+          src="/images/arrow_double_down.png"
+          alt="Select a frame"
+          layout="fill"
+          objectFit="contain"
+          priority
+        />
+      </Box>
     </Box>
   );
 };
