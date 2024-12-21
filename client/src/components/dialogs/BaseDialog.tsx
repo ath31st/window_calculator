d5007185d15edafc27e41efc8ae4079331c209b6
ath31st <ath31st@yandex.ru>
2024-12-21 10:09:21 +0300

    add base dialog with styles

---
+++ client/src/components/dialogs/BaseDialog.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@mui/material';
+
+interface BaseDialogProps {
+  isOpen: boolean;
+  title: string;
+  onClose: () => void;
+  children: ReactNode;
+  actions?: ReactNode;
+  contentStyles?: object;
+}
+
+const BaseDialog: React.FC<BaseDialogProps> = ({
+  isOpen,
+  title,
+  onClose,
+  children,
+  actions,
+  contentStyles,
+}) => {
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent
+        sx={{
+          padding: 3,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          overflow: 'visible',
+          minWidth: 300,
+          ...contentStyles,
+        }}
+      >
+        {children}
+      </DialogContent>
+      {actions && <DialogActions>{actions}</DialogActions>}
+    </Dialog>
+  );
+};
+
+export default BaseDialog;
