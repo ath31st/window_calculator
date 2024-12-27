d35eef61bebae72860d0772e42f43fca6ccfbe1f
ath31st <ath31st@yandex.ru>
2024-12-27 13:11:04 +0300

    update page title header with style

--- client/src/components/headers/PageHeader.tsx
+++ client/src/components/headers/PageHeader.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/material';
+import './page.header.css';
 
 interface PageHeaderProps {
   title: string;
@@ -7,7 +8,14 @@ interface PageHeaderProps {
 const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Typography variant="h3">{title}</Typography>
+      <div className="page-header">
+        <Typography
+          variant="h2"
+          className="title-text"
+        >
+          {title}
+        </Typography>
+      </div>
     </Box>
   );
 };
