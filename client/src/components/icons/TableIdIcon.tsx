406042aa385b433fe51e2447faeaceff8f5acd7f
ath31st <ath31st@yandex.ru>
2025-01-22 12:40:37 +0300

    extend table icon id for values more then 100

--- client/src/components/icons/TableIdIcon.tsx
+++ client/src/components/icons/TableIdIcon.tsx
@@ -1,5 +1,5 @@
 // components/TableIdIcon.tsx
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import theme from '@/app/_theme/theme';
 
 interface TableIdIconProps {
@@ -16,16 +16,22 @@ const TableIdIcon: React.FC<TableIdIconProps> = ({ id, type }) => {
             ? theme.palette.secondary.main
             : theme.palette.primary.main,
         color: 'white',
-        borderRadius: '50%',
-        width: { xs: 16, sm: 20, md: 24 },
+        borderRadius: '30%',
+        width: { xs: 20, sm: 24, md: 30 },
         height: { xs: 16, sm: 20, md: 24 },
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: { xs: '0.75rem', sm: '0.875rem', md: '1rem' },
       }}
     >
-      {id}
+      <Typography
+        variant="subtitle2"
+        sx={{
+          lineHeight: 1,
+        }}
+      >
+        {id}
+      </Typography>
     </Box>
   );
 };
