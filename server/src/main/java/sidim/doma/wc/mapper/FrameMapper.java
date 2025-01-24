7fd6606fe0b78f4b7bfa4a12d9dc668a472773b2
ath31st <ath31st@yandex.ru>
2025-01-24 13:58:34 +0300

    update FrameMapper after extending frame entity

--- server/src/main/java/sidim/doma/wc/mapper/FrameMapper.java
+++ server/src/main/java/sidim/doma/wc/mapper/FrameMapper.java
@@ -2,7 +2,9 @@ package sidim.doma.wc.mapper;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import sidim.doma.wc.dto.frame.FrameDto;
 import sidim.doma.wc.dto.frame.FrameFullDto;
+import sidim.doma.wc.dto.frame.NewFrameDto;
 import sidim.doma.wc.entity.Frame;
 
 @Component
@@ -10,10 +12,23 @@ import sidim.doma.wc.entity.Frame;
 public class FrameMapper {
   private final FrameBlockMapper frameBlockMapper;
 
+  public Frame fromNewDtoToEntity(NewFrameDto dto) {
+    return Frame.builder()
+        .id(null)
+        .name(dto.name())
+        .order(dto.order())
+        .build();
+  }
+
+  public FrameDto fromEntityToDto(Frame entity) {
+    return new FrameDto(entity.getId(), entity.getName(), entity.getOrder());
+  }
+
   public FrameFullDto fromEntityToFullDto(Frame entity) {
     return new FrameFullDto(
         entity.getId(),
         entity.getName(),
+        entity.getOrder(),
         entity.getFrameBlocks().stream()
             .map(frameBlockMapper::fromEntityToFullDto)
             .toList()
